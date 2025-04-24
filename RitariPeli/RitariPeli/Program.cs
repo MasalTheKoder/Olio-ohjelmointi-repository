e37754e80ea1c216fcb6ffbc6e448a92c49fe941
MasalTheKoder <s2201207@edu.bc.fi>
2025-04-25 02:29:35 +0300

    Ritari peli / Main menu

---
+++ RitariPeli/RitariPeli/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RitariPeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("=== RITARI PELI ===");
+                Console.WriteLine("1. Aloita taistelu.");
+                Console.WriteLine("2. Käy kaupassa.");
+                Console.WriteLine("3. Lopeta peli");
+                Console.Write("Valintasi: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        EnterBattle();
+                        break;
+                    case "2":
+                        VisitShop();
+                        break;
+                    case "3":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Virheellinen valinta");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+
+        static void EnterBattle()
+        {
+            Console.Clear();
+            Console.WriteLine("Taistelu alkaa!");
+            Console.WriteLine("Paina Enter palataksesi menuun.");
+            Console.ReadLine();
+        }
+
+        static void VisitShop()
+        {
+            Console.Clear();
+            Console.WriteLine("Tervetuloa Kauppaan!");
+            Console.WriteLine("Paina Enter palataksesi menuun");
+            Console.ReadLine();
+        }
+    }
+}
